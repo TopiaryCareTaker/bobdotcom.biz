39f6435186ab7d964eaac66765353f711ec74703
TopiaryCareTaker <arledgemike@gmail.com>
2021-07-23 15:55:31 -0500

    xxx | decent start. make bg animate on click than add more content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './index.scss';
-
-//import tellIt from './resources/scripts/page.js';
+import 'topiary-tools/scss/index.scss';
 
 import './resources/images/headerImage.png';
 import './resources/images/bob_back2themoney1.jpg';
@@ -8,5 +7,4 @@ import './resources/images/sectionOneBg.jpg';
 import './resources/images/bob_futureofbusiness.jpg';
 import './resources/images/bob_billuminati.gif';
 import './resources/images/favicon.png';
-
-//tellIt("Hello World");
+import './resources/scripts/page.js';
--- src/resources/scripts/page.js
+++ src/resources/scripts/page.js
@@ -1,5 +1,28 @@
-function tellIt(msg){
-    window.alert(msg);
+const backgrounds = [
+  'full-bleed',
+  'right',
+  'left',
+  'top',
+  'bottom'
+];
+
+let currentBackgroundIndex = 0;
+
+let currentBackgroundString =
+`container__background--${backgrounds[currentBackgroundIndex]}`;
+
+let nextBackgroundString = `container__background--${backgrounds[currentBackgroundIndex + 1]}`;
+
+const backgroundElem = document.querySelector('.container__background');
+
+backgroundElem.classList.add(currentBackgroundString);
+
+const changeBackground = (e)=>{
+  backgroundElem.classList.remove(currentBackgroundString);
+  backgroundElem.classList.add(nextBackgroundString);
+  
+  currentBackgroundIndex = currentBackgroundIndex + 1;
+  window.alert(currentBackgroundIndex)
 }
 
-export default tellIt;
+backgroundElem.addEventListener('click', changeBackground);
